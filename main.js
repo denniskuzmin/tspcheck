5319209f112d01320a7bcc40359eb4539cf2b71a
sample <megadesigner@gmail.com>
2017-02-05 10:38:05 +0500

    no message

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
+
 var d3 = require("d3");
 var quadtree = d3.quadtree();
 
 var fs = require('fs');
 var tspData;
 
-fs.readFile('sahara', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  tspData=data;
-  processFile(); 
-});
+tspData = fs.readFileSync('sahara','utf8');
 
-function processFile() {
-    console.log('data');
-    console.log(tspData);
-}
+
+
+
